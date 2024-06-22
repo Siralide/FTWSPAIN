d92b8b8326286b3b4343bbc66a49115b2ebd2102
Siralide <106344186+Siralide@users.noreply.github.com>
2024-06-22 12:30:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function scrollToSection(sectionId) {
-    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
-}
 
-document.querySelector('.contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Mensaje enviado correctamente!');
-    this.reset();
-});
+      // Función para hacer scroll a una sección específica
+      function scrollToSection(sectionId) {
+        document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+      }
+  
+      // Escucha el evento de submit en el formulario de contacto
+      document.querySelector('.contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        alert('Mensaje enviado correctamente!');
+        this.reset();
+      });
